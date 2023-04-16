8a1dda8a56692d757acd426eb8c1067d09be86c5
Francesco Abate <abatefrancesco98@gmail.com>
2023-04-16 23:07:41 +0200

    Added spawnpoints and game manager for beach volley game

---
+++ Assets/Scenes/Games/Beach Volley/BeachVolleyGameController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeachVolleyGameController : GameManager
+{
+    public override void OnPlayerDies()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnPlayerSpawns()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void FixedUpdateGameSpecificBehaviour()
+    {
+
+    }
+
+    protected override void OnRoomStarts()
+    {
+
+    }
+
+    protected override void UpdateGameSpecificBehaviour()
+    {
+
+    }
+}
