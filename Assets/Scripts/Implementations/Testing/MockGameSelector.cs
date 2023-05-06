28dac20c2e712bf503cd0f3e3383449395ef3ffb
Francesco Abate <abatefrancesco98@gmail.com>
2023-05-06 18:17:49 +0200

    Bomb Tag set for minimum 4 players

--- Assets/Scripts/Implementations/Testing/MockGameSelector.cs
+++ Assets/Scripts/Implementations/Testing/MockGameSelector.cs
@@ -80,7 +80,7 @@ public class MockGameSelector : MonoBehaviour, IGameSelector
                 supportedPlayers = new int[] { 2, 3, 4, 5, 6, 7, 8 };
                 break;
             case Constants.GameName.BombTag:
-                supportedPlayers = new int[] { 2, 3, 4, 5, 6, 7, 8 };
+                supportedPlayers = new int[] { 4, 5, 6, 7, 8 };
                 break;
             default:
                 throw new System.NullReferenceException("Missing game inside the game selection");
