1bfca44424b2aff1cdcb62060e13ae55c9888d99
Francesco Abate <abatefrancesco98@gmail.com>
2023-05-14 18:45:19 +0200

    Added Trap Run to menu launcher

--- Assets/Scripts/Implementations/Testing/MockGameLoader.cs
+++ Assets/Scripts/Implementations/Testing/MockGameLoader.cs
@@ -73,6 +73,9 @@ public class MockGameLoader : MonoBehaviour, IGameLoader
             case Constants.GameName.SuperHot:
                 sceneName += "superhot";
                 break;
+            case Constants.GameName.TrapRun:
+                sceneName += "traprun";
+                break;
             default:
                 throw new System.NullReferenceException($"No game named {game} is registered inside the loading component");
         }
